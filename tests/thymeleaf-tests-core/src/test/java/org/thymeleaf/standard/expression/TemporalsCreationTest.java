4a94aa8d42e24e544ca9092c4d086f580dcf1f16
Daniel Fernández <daniel@danfenz.dev>
2024-12-17 00:01:39 +0100

    Updated headers

--- tests/thymeleaf-tests-core/src/test/java/org/thymeleaf/standard/expression/TemporalsCreationTest.java
+++ tests/thymeleaf-tests-core/src/test/java/org/thymeleaf/standard/expression/TemporalsCreationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The THYMELEAF team.
+ * Copyright (c) 2011-2025 Thymeleaf (http://www.thymeleaf.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
